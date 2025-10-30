6c64403b02bafea89af22d8208bd8913867e7e17
Sangwoo Oh <epscag@inf.elte.hu>
2025-10-30 11:45:54 +0100

    実装済みの認証機能に合わせて、カラム名をpassword_hashからpasswordに変更。ER図、シードファイルも変更。

--- server/laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email');
-            $table->string('password_hash');
+            $table->string('password');
             $table->string('name');
             $table->enum('role',['admin','user']);
             $table->timestamps();
