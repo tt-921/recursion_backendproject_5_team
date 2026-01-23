86fcb232a3708d0e359c983630b014c179238fd5
kento-ix <kentoix1105@gmail.com>
2026-01-22 20:54:32 -0800

    test: Priceをfactory経由で作成するように修正

--- server/laravel/routes/api.php
+++ server/laravel/routes/api.php
@@ -3,12 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
-use App\Http\Controllers\CartController;
+
 
 Route::post('/stripe/webhook', [WebhookController::class, 'handle']);
-
-// カート
-Route::get('/cart',    [CartController::class, 'index']);
-Route::post('/cart',   [CartController::class, 'store']);
-Route::put('/cart',    [CartController::class, 'update']);
-Route::delete('/cart', [CartController::class, 'destroy']);
