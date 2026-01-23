af8149a46c24cf6642987a7c7231d64f708b6ea3
HirokiNakano <baseballinobu@gmail.com>
2026-01-24 00:49:17 +0900

    api/api/ならないように

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL!;
+export const API_URL = `${import.meta.env.VITE_API_URL!}/api`;
 export const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY!;
